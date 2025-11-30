52a8923e13cc3cb6ba60902cc893b0bbf3d251a8
daffz <dazht156@gmail.com>
2025-11-30 15:14:33 +0700

    Add files via upload

---
+++ web_servo_control.ino
@@ -0,0 +1,87 @@
+#include <WiFi.h>
+#include <Servo.h>
+
+Servo myServo;
+
+const char* ssid = "YOUR_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
+
+WiFiServer server(80);
+
+String header;
+
+String valueString = String(5);
+int pos1 = 0;
+int pos2 = 0;
+
+void setup() {
+  Serial.begin(115200);
+  myServo.attach(13);
+  myServo.write(0);
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+void loop(){
+  WiFiClient client = server.available();
+
+  if (client) {
+    String currentLine = "";
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        header += c;
+        if (c == '\n') {
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            client.println("<style>body { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".slider { width: 300px; }</style>");
+            client.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>");
+            client.println("</head><body><h1>ESP32 Servo Control</h1>");
+            client.println("<p>Position: <span id=\"servoPos\"></span></p>");
+            client.println("<input type=\"range\" min=\"0\" max=\"180\" class=\"slider\" id=\"servoSlider\" onchange=\"servo(this.value)\" value=\""+valueString+"\"/>");
+            client.println("<script>var slider = document.getElementById(\"servoSlider\");");
+            client.println("var servoP = document.getElementById(\"servoPos\"); servoP.innerHTML = slider.value;");
+            client.println("slider.oninput = function() { slider.value = this.value; servoP.innerHTML = this.value; }");
+            client.println("$.ajaxSetup({timeout:1000}); function servo(pos) { ");
+            client.println("$.get(\"/?value=\" + pos + \"&\"); {Connection: close};}</script>");
+            client.println("</body></html>");
+
+            client.println();
+            break;
+          } else {
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;
+        }
+        if (currentLine.endsWith("GET /?value=")) {
+          pos1 = currentLine.indexOf('=');
+          pos2 = currentLine.indexOf(' ', pos1);
+          valueString = currentLine.substring(pos1 + 1, pos2);
+          myServo.write(valueString.toInt());
+        }
+      }
+    }
+    header = "";
+    client.stop();
+  }
+}
